439ebd5552823f7110f5888324eed11e4ab3aed6
Jiaowenhui <523947655@qq.com>
2018-02-04 14:06:59 +0800

    修改没有图片页面bug

--- deploy/srv/routes/index.js
+++ deploy/srv/routes/index.js
@@ -4,10 +4,11 @@ const Index = require('../models/index.js');
 
 /* GET home page. */
 router.get('/', Index.index);
-router.get('/navmain/:nav', Index.navmain);
-router.get('/album', Index.albumindex);
+// router.get('/navmain/:nav', Index.navmain);
 router.get('/album/:nav', Index.navmain);
+router.get('/album', Index.albumindex);
 router.get('/album/:nav/:albumid/:imgname', Index.album);
+router.get('/notalbum/:nav', Index.notalbum);
 router.get('/novel', Index.novel);
 
 
